8956673b0aecdcb7064da3ebee103aa189ee1377
Purva Khandekar <purvakhandekar1208@gmail.com>
2026-07-14 21:09:05 +0530

    Added MongoDB connection andUser model

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,28 @@
 const express = require("express");
 const cors = require("cors");
-require("dotenv").config();
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+// Load environment variables
+dotenv.config();
+
+// Connect to MongoDB
+connectDB();
 
 const app = express();
 
+// Middleware
 app.use(cors());
 app.use(express.json());
 
-const PORT = process.env.PORT || 5000;
-
+// Test Route
 app.get("/", (req, res) => {
-    res.send("🚀 Power Pulse AI Backend Running...");
+    res.send("🚀 Power Pulse AI Backend is Running...");
 });
 
+// Server
+const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`🚀 Server is running on port ${PORT}`);
 });
